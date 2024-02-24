26eed04f642826cb46b9c4a28f1ddb336a70f346
Soso <soso.beriashvili@redberry.ge>
2024-02-24 17:08:30 +0400

    feat: add tailwind and finish login markup

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,58 +4,74 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>Task Manager</title>
-        <link
-            href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
-            rel="stylesheet"
-        />
+        @vite('resources/css/app.css')
     </head>
-    <body class="flex items-center justify-center">
-        <section class="flex items-center justify-center h-screen w-4/5">
-            <div class="flex-1 flex justify-end">
-                <img
-                    class="self-end"
-                    src="/images/Intersect.png"
-                    alt="Login cover"
-                />
-            </div>
-            <div class="flex-1 flex items-center justify-center">
-                <div class="w-4/5">
-                    <div class="flex items-center justify-between pb-7">
-                        <div>
-                            <h1 class="text-4xl">
-                                {{ mb_strtoupper("welcome back!") }}
-                            </h1>
-                            <span class="text-base text-gray-600"
-                                >Please, enter your details</span
+    <body>
+        <section class="flex items-center justify-center">
+            <div class="flex items-center justify-center h-screen w-4/5 py-6">
+                <div class="flex-1 flex justify-end">
+                    <img
+                        class="self-end"
+                        src="/images/Intersect.png"
+                        alt="Login cover"
+                    />
+                </div>
+                <div
+                    class="flex-1 flex items-center justify-center flex-col h-screen pb-20"
+                >
+                    <div class="w-4/5 mt-auto">
+                        <div class="flex items-center justify-between pb-7">
+                            <div>
+                                <h1 class="text-4xl font-bold mb-1">
+                                    {{ mb_strtoupper("welcome back!") }}
+                                </h1>
+                                <span class="text-base text-gray-600"
+                                    >Please, enter your details</span
+                                >
+                            </div>
+                            <div>
+                                <img src="/images/Group.png" alt="Smile icon" />
+                            </div>
+                        </div>
+                        <form method="POST" action="#">
+                            <div class="flex gap-4 flex-col py-4">
+                                <div>
+                                    <input
+                                        class="placeholder-gray-400 px-4 py-6 border outline-none focus:border-blue-500 w-full h-16 bg-gray-100 rounded-3xl"
+                                        type="email"
+                                        placeholder="E-mail"
+                                    />
+                                </div>
+                                <div class="relative">
+                                    <input
+                                        class="placeholder-gray-400 px-4 py-6 border outline-none focus:border-blue-500 w-full h-16 bg-gray-100 rounded-3xl"
+                                        type="password"
+                                        placeholder="Write your Password"
+                                    />
+                                    <div
+                                        class="absolute right-0 top-1/2 transform -translate-x-full -translate-y-1/2"
+                                    >
+                                        <img
+                                            class="cursor-pointer"
+                                            src="/images/eye.png"
+                                            alt="Eye icon"
+                                        />
+                                    </div>
+                                </div>
+                            </div>
+
+                            <button
+                                class="transition-all hover:bg-blue-400 w-full h-16 rounded-3xl bg-blue-500 text-white text-base"
+                                type="submit"
                             >
-                        </div>
-                        <div>
-                            <img src="/images/Group.png" alt="Smile icon" />
-                        </div>
+                                LOG IN
+                            </button>
+                        </form>
                     </div>
-                    <form method="POST" action="#">
-                        <div class="flex gap-4 flex-col py-4">
-                            <div>
-                                <input
-                                    class="w-full h-16 bg-gray-100 rounded-3xl"
-                                    type="email"
-                                    placeholder="E-mail"
-                                />
-                            </div>
-                            <div>
-                                <input
-                                    class="w-full h-16 bg-gray-100 rounded-3xl"
-                                    type="password"
-                                />
-                            </div>
-                        </div>
-                        <button
-                            class="w-full h-16 rounded-3xl bg-blue-500 text-white text-base"
-                            type="submit"
-                        >
-                            LOG IN
-                        </button>
-                    </form>
+                    <div class="mt-auto flex gap-12">
+                        <span class="cursor-pointer">English</span>
+                        <span class="cursor-pointer">ქართული</span>
+                    </div>
                 </div>
             </div>
         </section>
