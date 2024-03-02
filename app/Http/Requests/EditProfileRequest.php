5b076dea922d090eb55c7c677c6806b7e87778e2
Soso <soso.beriashvili@redberry.ge>
2024-03-02 14:01:43 +0400

    chore: add validation rules for the edit page in the form request

--- app/Http/Requests/EditProfileRequest.php
+++ app/Http/Requests/EditProfileRequest.php
@@ -6,10 +6,14 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class EditProfileRequest extends FormRequest
 {
-   public function rules(): array
-    {
-        return [
-            //
-        ];
-    }
+	public function rules(): array
+	{
+		return [
+			'current_password'      => 'required_with:password,password_confirmation|current_password:api',
+			'password'              => 'nullable|required_with:password_confirmation|min:4',
+			'password_confirmation' => 'nullable|required_with:password|confirmed',
+			'profile_picture'       => 'nullable|image',
+			'cover_picture'         => 'nullable|image',
+		];
+	}
 }
