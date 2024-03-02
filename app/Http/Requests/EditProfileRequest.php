13c9191c4621df1b74e07b4b6cfe324743512372
Soso <soso.beriashvili@redberry.ge>
2024-03-02 13:51:57 +0400

    chore: create a form request for the profile page, make changes to the form structure

---
+++ app/Http/Requests/EditProfileRequest.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class EditProfileRequest extends FormRequest
+{
+   public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
--- resources/views/components/profile-photo.blade.php
+++ resources/views/components/profile-photo.blade.php
@@ -1,14 +1,27 @@
-@props(['src'])
+@props(['src', 'name']) 
+
+@php 
+
+$buttonClasses = 'cursor-pointer flex flex-shrink-0 items-center
+justify-center gap-2 border border-blue-400 rounded-lg py-3 px-6 font-bold
+text-blue-400 hover:bg-blue-300 hover:text-white hover:border-white
+transition-all duration-300'; 
+
+@endphp
 
 <div class="flex items-center gap-10">
     <div class="w-32">
         <img src="{{ $src }}" class="w-full" alt="Profile picture" />
     </div>
-    <button
-        class="flex flex-shrink-0 items-center justify-center gap-2 border border-blue-400 rounded-lg py-3 px-6 font-bold text-blue-400 hover:bg-blue-300 hover:text-white hover:border-white transition-all duration-300"
-    >
+
+    <input class="hidden" type="file" id="{{ $name }}" name="{{ $name }}"
+    accept="image/*"">
+
+    <label {{ $attributes(['class' => $buttonClasses]) }}  for="{{ $name }}">
         <img src="{{ asset('/images/upload.png') }}" alt="Plus icon" />
-        {{ $slot }}
-    </button>
-    <x-table.table-button class="border-none">{{ __('profile.delete') }}</x-table.table-button>
+        {{ $slot }}</label
+    >
+    <x-table.table-button class="border-none">{{
+        __("profile.delete")
+    }}</x-table.table-button>
 </div>
