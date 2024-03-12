3f95dc17b5cf768c308fc55ad32578b15b92c85f
Soso <soso.beriashvili@redberry.ge>
2024-03-12 22:25:05 +0400

    fix: fix languages' background and profile page styles issue

--- resources/views/components/form/label.blade.php
+++ resources/views/components/form/label.blade.php
@@ -1,7 +1,7 @@
 @props(['name', 'placeholder'])
 
 @php
-    $classes = 'absolute left-4 text-base text-gray-400 transition-all duration-300 transform -translate-y-1/2 origin-top pointer-events-none';
+    $classes = 'text-black absolute left-4 text-base transition-all duration-300 transform -translate-y-1/2 origin-top pointer-events-none';
 @endphp
 
 <label
