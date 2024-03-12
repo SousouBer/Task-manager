26259552fc2637f0b2c35f6c36e4de0b506febe2
Soso <soso.beriashvili@redberry.ge>
2024-03-12 19:14:02 +0400

    fix: add favicon

--- resources/views/components/main-layout.blade.php
+++ resources/views/components/main-layout.blade.php
@@ -4,6 +4,7 @@
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+        <link rel="icon" type="image/x-icon" href="{{ asset('/images/components/favicon.svg')}}">
         <title>Task Manager</title>
         @vite('resources/css/app.css')
     </head>
